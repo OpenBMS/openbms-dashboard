ab0fb5abd1e5f071d0604de092abf544d0358b79
aperugini <alexandre.perugini@gmail.com>
2016-02-19 06:28:28 -0500

    Improved styling, positioning and fixed real time positioning.

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,10 +1,25 @@
 $(document).ready(function(){
-  $('.info-panel').click(function(){
-    var pwidth = $(this).width
-    var pheight = $(this).height
-
-    $(this).children('.popover').toggle();
+  var popoverPos = {
+    "battery-lamp-plug": "left",
+    "battery-plug": "bottom",
+    "fan-plug": "left",
+    "fan-arduino": "top",
+    "lamp-plug": "right",
+    "battery-arduino": "left"
+  };
+  $(".info-panel").each(function(){
+    var id = $(this).attr("id");
+    $(this).children(".popover").removeClass().addClass("popover fade in " + popoverPos[id]);
   });
+  $('.info-panel').click(function(event){
+    if (event.target === this){
+      var pwidth = $(this).width
+      var pheight = $(this).height
+
+      $('.popover').toggle();
+    }
+  });
+
   $('.commands').submit(function(event){
     event.preventDefault();
     var url = $(this).attr('action');
@@ -30,22 +45,46 @@ $(document).ready(function(){
       $.get( "dashboard_data", function( data ) {
           var disruption = data.disruption == false ? 'Normal' : 'Loss of Generation';
           $('#status').html(disruption);
-          $('#total_power').html(data.total_power.magnitude + ' ' + data.total_power.unit)
+          $('#total_power').html(data.total_power.magnitude.toFixed(2) + ' ' + data.total_power.unit)
+          $('#price').html(data.price.toFixed(1) + ' ¢/kWh');
+          var priceClass = 'panel panel-info';
+          if(data.price > data.thresholds.low){
+            priceClass = 'panel panel-warning';
+          }
+          if(data.price > data.thresholds.high ){
+            priceClass = 'panel panel-success';
+          }
+          $('#thresholds').removeClass().addClass(priceClass);
           for(device in data.entities){
-              var content = $('#' + device).find('.popover-content');
+              devData = data.entities[device];
+              var content = $('#' + devData['location']).find('.popover-content');
               content.html('');
-              for(attr in data.entities[device]){
+              for(attr in devData){
                   if (attr != 'location' && attr != 'device_type'){
-                      if (attr == 'power' || attr == 'voltage'){
-                          content.prepend(attr + ': ' + data.entities[device][attr].magnitude + ' ' + data.entities[device][attr].unit + '</br>');
+                      if(attr == 'speed' ){
+                          $('#' + devData['location']).find('[data-property="speed"]').children().removeClass('btn-primary active').removeAttr('clicked');
+                          $('#' + devData['location']).find('[data-property="speed"]').children('[value=' + capitalizeFirstLetter(devData[attr]) +']')
+                              .addClass('btn-primary active').attr('clicked', 'true');
+                      }
+                      else if(attr == 'status'){
+                          $('#' + devData['location']).find('[data-property="status"]').children().removeClass('btn-primary active').removeAttr('clicked');
+                          $('#' + devData['location']).find('[data-property="status"]').children('[value=' + capitalizeFirstLetter(devData[attr]) +']')
+                              .addClass('btn-primary active').attr('clicked', 'true');
+                      }
+                      else if (attr == 'power' || attr == 'voltage'){
+                          content.prepend(capitalizeFirstLetter(attr) + ': ' + devData[attr].magnitude.toFixed(2) + ' ' + devData[attr].unit + '</br>');
                       }
                       else{
-                          content.prepend(attr + ': ' + data.entities[device][attr] + '</br>');
+                          content.prepend(capitalizeFirstLetter(attr) + ': ' + devData[attr] + '</br>');
                       }
                   }
               }
           }
       });
   }
-  setInterval(getDataFromWS, 5000)
+  setInterval(getDataFromWS, 2000);
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
 });
